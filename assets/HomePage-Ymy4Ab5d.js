316407b15c34ebee9d98e6178a3ac0b54a430153
Belodubrovsky Vitaly <vitaly@imageteq.com>
2026-06-10 23:36:33 +0300

    Updates

---
+++ assets/HomePage-Ymy4Ab5d.js
@@ -0,0 +1,1 @@
+import{j as r,M as a,T as e}from"./index-BP68iK3K.js";function n(){return r.jsx(r.Fragment,{children:r.jsx(a,{title:"Начало осмотра",children:r.jsx(e,{variant:"body2",children:"Начало осмотра."})})})}export{n as default};
